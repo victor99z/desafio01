52359f8e521e0fae900b16ef34a4e411e887cc50
Victor <victor_luizbernardes5@outlook.com>
2021-11-30 19:49:23 -0300

    resolvido problema de sincronia no .map
    Signed-off-by: Victor <victor_luizbernardes5@outlook.com>

--- valida.js
+++ valida.js
@@ -1,9 +1,9 @@
 const validaCampos = require("./validators/validaCampos")
 
+
 let stdin = process.stdin,
     stdout = process.stdout,
-    inputChunks = [],
-    parsedData = {};
+    inputChunks = [];
 
 stdin.resume();
 stdin.setEncoding('utf8');
@@ -12,20 +12,32 @@ stdin.on('data', function (chunk) {
     inputChunks.push(chunk);
 });
 
-stdin.on('end', function () {
+stdin.on('end', async function () {
     stdout.write("\n====================================\n")
     let inputJSON = inputChunks.join("");
     parsedDataArray = JSON.parse(inputJSON);
 
     let outputJson = []
 
-    parsedDataArray.map(parsedData => {
-        outputJson.push(validaCampos(parsedData))
-    })
+    /**
+     * Solução = usar o Promise.all para esperar todos os request's
+     */
 
-    //let output = validaCampos(parsedData)
-    outputJson = JSON.stringify(outputJson, null, '    ')
-    stdout.write(outputJson)
+    let output = await Promise.all(parsedDataArray.map(async (item) => {
+        let value = await validaCampos(item)
+        outputJson.push(value)
+    }))
+
+    /**
+     * Esse não funciona :<
+     */
+    // parsedDataArray.map(async (item) => {
+    //     let value = await validaCampos(item)
+    //     outputJson.push(value)
+    // })
+
+    output = JSON.stringify(outputJson, null, '    ')
+    stdout.write(output)
 });
 
 
